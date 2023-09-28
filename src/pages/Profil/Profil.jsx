177a794702d2363d22e59e66003546c474af7da4
AsuraMoon <73102506+AsuraMoon@users.noreply.github.com>
2023-09-28 12:59:24 +0200

    Mocked not working for now

--- src/pages/Profil/Profil.jsx
+++ src/pages/Profil/Profil.jsx
@@ -1,96 +1,104 @@
-import { useParams } from 'react-router-dom'; // Importation du hook useParams pour récupérer les paramètres d'URL.
-import { useFetch } from '../../services/useFetch'; // Importation de la fonction useFetch depuis le service.
-
-import './Profil.scss'; // Importation des styles CSS pour le composant Profil.
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import './Profil.scss';
 
 function Profil() {
-	// Définition du titre de la page.
-	document.title = 'Profil - SportSee';
+  // Définition du titre de la page.
+  useEffect(() => {
+    document.title = 'Profil - SportSee';
+  }, []);
 
-	const { id } = useParams(); // Récupération de l'ID de l'utilisateur depuis les paramètres d'URL.
-	const baseCall = `http://localhost:3000/user/${id}`; // URL de base pour les appels à l'API.
-	const baseMocked = `../../../public/mockedData/`; // Chemin de base pour les données simulées.
+  const { id } = useParams(); // Récupération de l'ID de l'utilisateur depuis les paramètres d'URL.
+  const baseCall = `http://localhost:3000/user/${id}`;
+  const baseMocked = `../../../public/mockedData/`;
 
-	/* Appel à la fonction useFetch pour récupérer les données depuis l'API ou les données simulées. */
-	const user = useFetch(
-		baseCall,
-		id,
-		baseMocked + `user-activity.json`
-	);
-	const activity = useFetch(
-		baseCall + `/activity`,
-		id,
-		baseMocked + `user-activity.json`
-	);
-	const averageSessions = useFetch(
-		baseCall + `/average-sessions`,
-		id,
-		baseMocked + `user-average-sessions.json`
-	);
-	const performance = useFetch(
-		baseCall + `/performance`,
-		id,
-		baseMocked + `user-performance.json`
-	);
+  // Utilisation de useState pour gérer l'état des données.
+  const [userData, setUserData] = useState(null);
+  const [activityData, setActivityData] = useState(null);
+  const [averageSessionsData, setAverageSessionsData] = useState(null);
+  const [performanceData, setPerformanceData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-	/* Fonction pour formater les données à partir de l'API ou des données simulées. */
-	const formatData = (dataObject, apiData) => {
-		if (apiData.apiData) {
-			dataObject = apiData.apiData;
-			return dataObject;
-		} else if (apiData.mockedData) {
-			dataObject = apiData.mockedData;
-			return dataObject;
-		}
-	}
+  // Utilisation de useEffect pour effectuer les appels API au chargement du composant.
+  useEffect(() => {
+    async function fetchDataFromAPI() {
+      try {
+        const response = await fetch(baseCall);
+        if (!response.ok) {
+          throw new Error('Erreur réseau');
+        }
+        const data = await response.json();
+        setUserData(data.data);
 
-	/* Initialisation des objets de données et formatage des données. */
-	let userData = {};
-	userData = formatData(userData, user);
-	let activityData = {};
-	activityData = formatData(activityData, activity);
-	let averageSessionsData = {};
-	averageSessionsData = formatData(averageSessionsData, averageSessions);
-	let performanceData = {};
-	performanceData = formatData(performanceData, performance);
+        const activityResponse = await fetch(`${baseCall}/activity`);
+        const activityData = await activityResponse.json();
+        setActivityData(activityData);
 
-	/* Si les données sont en cours de chargement, affiche un message de chargement à l'utilisateur. */
-	if (
-		user.isLoading ||
-		activity.isLoading ||
-		averageSessions.isLoading ||
-		performance.isLoading
-	) {
-		return (
-			<section className="profil-wrapper">
-				<h2 className="center">Chargement...</h2>
-			</section>
-		);
-	}
+        const averageSessionsResponse = await fetch(`${baseCall}/average-sessions`);
+        const averageSessionsData = await averageSessionsResponse.json();
+        setAverageSessionsData(averageSessionsData);
 
-	/* Si les appels à l'API et aux données simulées renvoient des erreurs, affiche un message d'erreur à l'utilisateur. */
-	if (
-		(user.errorAPI && user.errorMocked) ||
-		(activity.errorAPI && activity.errorMocked) ||
-		(averageSessions.errorAPI && averageSessions.errorMocked) ||
-		(performance.errorAPI && performance.errorMocked)
-	) {
-		return (
-			<section className="profil-wrapper">
-				<h2 className="center">Une erreur est survenue !</h2>
-			</section>
-		);
-	}
+        const performanceResponse = await fetch(`${baseCall}/performance`);
+        const performanceData = await performanceResponse.json();
+        setPerformanceData(performanceData);
 
-	/* Affichage des données formatées dans un format lisible (JSON). */
-	return (
-		<>
-			<pre>{JSON.stringify(userData, null, 2)}</pre>
-			<pre>{JSON.stringify(activityData, null, 2)}</pre>
-			<pre>{JSON.stringify(averageSessionsData, null, 2)}</pre>
-			<pre>{JSON.stringify(performanceData, null, 2)}</pre>
-		</>
-	);
+        setLoading(false);
+      } catch (error) {
+        // En cas d'erreur, récupérez les données depuis le fichier mock.
+        try {
+          const mockResponse = await fetch(`${baseMocked}user-mock-data.json`);
+          const mockData = await mockResponse.json();
+          setUserData(mockData.data);
+
+          const mockActivityResponse = await fetch(`${baseMocked}user-activity-mock.json`);
+          const mockActivityData = await mockActivityResponse.json();
+          setActivityData(mockActivityData);
+
+          const mockAverageSessionsResponse = await fetch(`${baseMocked}user-average-sessions-mock.json`);
+          const mockAverageSessionsData = await mockAverageSessionsResponse.json();
+          setAverageSessionsData(mockAverageSessionsData);
+
+          const mockPerformanceResponse = await fetch(`${baseMocked}user-performance-mock.json`);
+          const mockPerformanceData = await mockPerformanceResponse.json();
+          setPerformanceData(mockPerformanceData);
+
+          setLoading(false);
+        } catch (error) {
+          setError('Une erreur est survenue');
+          setLoading(false);
+        }
+      }
+    }
+
+    fetchDataFromAPI();
+  }, [baseCall, baseMocked]);
+
+  // Affichage des données formatées dans un format lisible (JSON).
+  if (loading) {
+    return (
+      <section className="profil-wrapper">
+        <h2 className="center">Chargement...</h2>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className="profil-wrapper">
+        <h2 className="center">Une erreur est survenue !</h2>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <pre>{JSON.stringify(userData, null, 2)}</pre>
+      <pre>{JSON.stringify(activityData, null, 2)}</pre>
+      <pre>{JSON.stringify(averageSessionsData, null, 2)}</pre>
+      <pre>{JSON.stringify(performanceData, null, 2)}</pre>
+    </>
+  );
 }
 
 export default Profil; // Exportation du composant Profil.
