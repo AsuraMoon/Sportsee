01ad12761696192de27e90e6daef67a23c5dd6d7
AsuraMoon <73102506+AsuraMoon@users.noreply.github.com>
2023-09-28 12:50:34 +0200

    Ajout commentaire

--- src/pages/Profil/Profil.jsx
+++ src/pages/Profil/Profil.jsx
@@ -1,71 +1,60 @@
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'; // Importation du hook useParams pour récupérer les paramètres d'URL.
+import { useFetch } from '../../services/useFetch'; // Importation de la fonction useFetch depuis le service.
 
-import { useFetch } from '../../services/useFetch'
-
-/*import Card from '../../components/Card'
-import ChartsCard from '../../components/ChartsCard'
-import ChartActivity from '../../components/ChartActivity'
-import ChartAverageSessions from '../../components/ChartAverageSessions'
-import ChartGoal from '../../components/ChartGoal'
-import ChartPerformance from '../../components/ChartPerformance'
-
-import energy from '../../assets/energy.svg'
-import chicken from '../../assets/chicken.svg'
-import apple from '../../assets/apple.svg'
-import cheeseburger from '../../assets/cheeseburger.svg'*/
-
-import './Profil.scss'
+import './Profil.scss'; // Importation des styles CSS pour le composant Profil.
 
 function Profil() {
-	document.title = 'Profil - SportSee'
+	// Définition du titre de la page.
+	document.title = 'Profil - SportSee';
 
-	const {id} = useParams()
-  const baseCall = `http://localhost:3000/user/${id}`
-  const baseMocked = `../../../public/mockedData/`
+	const { id } = useParams(); // Récupération de l'ID de l'utilisateur depuis les paramètres d'URL.
+	const baseCall = `http://localhost:3000/user/${id}`; // URL de base pour les appels à l'API.
+	const baseMocked = `../../../public/mockedData/`; // Chemin de base pour les données simulées.
 
-	/* Fetch the data from API or mocked data */
+	/* Appel à la fonction useFetch pour récupérer les données depuis l'API ou les données simulées. */
 	const user = useFetch(
 		baseCall,
 		id,
-		baseMocked+`user-activity.json`
-	)
+		baseMocked + `user-activity.json`
+	);
 	const activity = useFetch(
-		baseCall+`/activity`,
+		baseCall + `/activity`,
 		id,
-		baseMocked+`user-activity.json`
-	)
+		baseMocked + `user-activity.json`
+	);
 	const averageSessions = useFetch(
-		baseCall+`/average-sessions`,
+		baseCall + `/average-sessions`,
 		id,
-    baseMocked+`user-average-sessions.json`
-	)
+		baseMocked + `user-average-sessions.json`
+	);
 	const performance = useFetch(
-		baseCall+`/performance`,
+		baseCall + `/performance`,
 		id,
-		baseMocked+`user-performance.json`
-	)
+		baseMocked + `user-performance.json`
+	);
 
+	/* Fonction pour formater les données à partir de l'API ou des données simulées. */
 	const formatData = (dataObject, apiData) => {
 		if (apiData.apiData) {
-			dataObject = apiData.apiData
-			return dataObject
+			dataObject = apiData.apiData;
+			return dataObject;
 		} else if (apiData.mockedData) {
-			dataObject = apiData.mockedData
-			return dataObject
+			dataObject = apiData.mockedData;
+			return dataObject;
 		}
 	}
 
-	/* Init the dataObject and format the data */
-	let userData = {}
-	userData = formatData(userData, user)
-	let activityData = {}
-	activityData = formatData(activityData, activity)
-	let averageSessionsData = {}
-	averageSessionsData = formatData(averageSessionsData, averageSessions)
-	let performanceData = {}
-	performanceData = formatData(performanceData, performance)
+	/* Initialisation des objets de données et formatage des données. */
+	let userData = {};
+	userData = formatData(userData, user);
+	let activityData = {};
+	activityData = formatData(activityData, activity);
+	let averageSessionsData = {};
+	averageSessionsData = formatData(averageSessionsData, averageSessions);
+	let performanceData = {};
+	performanceData = formatData(performanceData, performance);
 
-	/* If the data is loading, display a loading message to the user */
+	/* Si les données sont en cours de chargement, affiche un message de chargement à l'utilisateur. */
 	if (
 		user.isLoading ||
 		activity.isLoading ||
@@ -76,10 +65,10 @@ function Profil() {
 			<section className="profil-wrapper">
 				<h2 className="center">Chargement...</h2>
 			</section>
-		)
+		);
 	}
 
-	/* If the fetches on the API and the mocked data returns errors, display a error message to the user */
+	/* Si les appels à l'API et aux données simulées renvoient des erreurs, affiche un message d'erreur à l'utilisateur. */
 	if (
 		(user.errorAPI && user.errorMocked) ||
 		(activity.errorAPI && activity.errorMocked) ||
@@ -90,17 +79,18 @@ function Profil() {
 			<section className="profil-wrapper">
 				<h2 className="center">Une erreur est survenue !</h2>
 			</section>
-		)
+		);
 	}
 
+	/* Affichage des données formatées dans un format lisible (JSON). */
 	return (
 		<>
-      <pre>{JSON.stringify(userData, null, 2)}</pre>
-      <pre>{JSON.stringify(activityData, null, 2)}</pre>
-      <pre>{JSON.stringify(averageSessionsData, null, 2)}</pre>
-      <pre>{JSON.stringify(performanceData, null, 2)}</pre>
+			<pre>{JSON.stringify(userData, null, 2)}</pre>
+			<pre>{JSON.stringify(activityData, null, 2)}</pre>
+			<pre>{JSON.stringify(averageSessionsData, null, 2)}</pre>
+			<pre>{JSON.stringify(performanceData, null, 2)}</pre>
 		</>
-	)
+	);
 }
 
-export default Profil
+export default Profil; // Exportation du composant Profil.
